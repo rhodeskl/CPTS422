e507a936e46fb25135b8e01d1dd0fa1a796eb8f1
krhodes <kayla.rhodes@wsu.edu>
2018-09-19 13:28:11 -0700

    made change to logic of comment counter

--- structural-metrics/src/main/java/cpts422/structural_metrics/CommentCounter.java
+++ structural-metrics/src/main/java/cpts422/structural_metrics/CommentCounter.java
@@ -3,26 +3,19 @@ package cpts422.structural_metrics;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import java.util.ArrayList;
 
 public class CommentCounter extends AbstractCheck {
 	private int numComments;
 	private int numCommentLines;
+	private ArrayList<Integer> beginBlockCommentLines;
+	private ArrayList<Integer> endBlockCommentLines;
 	
 	public CommentCounter() {
 		this.numCommentLines = 0;
 		this.numComments = 0;
-	}
-	
-	public void countComments(DetailAST ast) {
-		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
-		int numSingleComments = objBlock.getChildCount(TokenTypes.SINGLE_LINE_COMMENT);
-		int numBlockComments = objBlock.getChildCount(TokenTypes.BLOCK_COMMENT_BEGIN);
-		numComments = numSingleComments + numBlockComments;
-		log(ast, "Number of Comments = " + numComments);
-	}
-	
-	public void countLines(DetailAST ast) {
-		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+		this.beginBlockCommentLines = new ArrayList<Integer>();
+		this.endBlockCommentLines = new ArrayList<Integer>();
 	}
 	
 	public int getNumComments() {
@@ -32,25 +25,51 @@ public class CommentCounter extends AbstractCheck {
 	public int getCommentLines() {
 		return numCommentLines;
 	}
+	
+	@Override
+	public void beginTree(DetailAST rootAST) {
+		numComments = 0;
+		numCommentLines = 0;
+	}
 
 	@Override
 	public int[] getAcceptableTokens() {
-		return new int[] {TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+		return getDefaultTokens();
 	}
 
 	@Override
 	public int[] getDefaultTokens() {
-		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+		return new int[] { TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.BLOCK_COMMENT_END, TokenTypes.SINGLE_LINE_COMMENT };
 	}
 
 	@Override
 	public int[] getRequiredTokens() {
-		return new int[0];
+		return getDefaultTokens();
 	}
 	
 	@Override
 	public void visitToken(DetailAST ast) {
-		countComments(ast);
-		countLines(ast);
+		switch (ast.getType()) {
+		case TokenTypes.BLOCK_COMMENT_BEGIN:
+			numComments++;
+			beginBlockCommentLines.add(ast.getLineNo());
+		case TokenTypes.BLOCK_COMMENT_END:
+			endBlockCommentLines.add(ast.getLineNo());
+		case TokenTypes.SINGLE_LINE_COMMENT:
+			numComments++;
+			numCommentLines++;
+		}
+	}
+	
+	@Override
+	public void finishTree(DetailAST rootAST) {
+		for(int i = 0; i < beginBlockCommentLines.size(); i++) {
+			Integer lineNoBegin = beginBlockCommentLines.get(i);
+			Integer lineNoEnd = endBlockCommentLines.get(i);
+			Integer numLines = lineNoEnd - lineNoBegin + 1;
+			numCommentLines = numLines + numCommentLines;
+		}
+		log(rootAST, "Number of comments = " + numComments);
+		log(rootAST, "Number of lines of comments = " + numCommentLines);
 	}
 }
