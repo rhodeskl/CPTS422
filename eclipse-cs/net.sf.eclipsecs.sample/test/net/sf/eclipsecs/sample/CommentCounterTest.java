3c2c8ab8cbb8f0ee1837539ae5de422257496769
krhodes <kayla.rhodes@wsu.edu>
2018-10-10 13:49:43 -0700

    added test function declarations for comment counter

--- eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/CommentCounterTest.java
+++ eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/CommentCounterTest.java
@@ -1,5 +1,61 @@
 package net.sf.eclipsecs.sample;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sf.eclipsecs.sample.checks.CommentCounter;
+
 public class CommentCounterTest {
+  CommentCounter commentCounter;
+  
+  @Before
+  public void setup() {
+    commentCounter = new CommentCounter();
+  }
+  
+  @Test
+  public void testGetNumComments() {
+    
+  }
+  
+  @Test
+  public void testGetNumLines() {
+    
+  }
+  
+  @Test
+  public void testIsCommentNodesRequired() {
+    
+  }
+  
+  @Test
+  public void testBeginTree() {
+    
+  }
+  
+  @Test
+  public void testGetAcceptableTokens() {
+    
+  }
+  
+  @Test
+  public void testGetDefaultTokens() {
+    
+  }
+  
+  @Test
+  public void testGetRequiredTokens() {
+    
+  }
+  
+  @Test
+  public void testVisitToken() {
+    
+  }
+  
+  @Test
+  public void testFinishTree() {
+    
+  }
 
 }
