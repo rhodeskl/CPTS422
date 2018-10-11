3e29b2550895298f969d2af8c2097c2d5f368242
krhodes <kayla.rhodes@wsu.edu>
2018-10-11 11:24:02 -0700

    finished unit tests for CommentCounter and VariableCounter

--- eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/CommentCounterTest.java
+++ eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/CommentCounterTest.java
@@ -4,48 +4,64 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import net.sf.eclipsecs.sample.checks.CommentCounter;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DetailAST.class)
 public class CommentCounterTest {
-  CommentCounter commentCounter;
+  private CommentCounter commentCounter;
+  private DetailAST astMock;
   
   @Before
   public void setup() {
     commentCounter = new CommentCounter();
+    astMock = PowerMockito.mock(DetailAST.class);
   }
   
   @Test
   public void testGetNumComments() {
-    
+    //getNumComments should return the number of comments
+    //in this case, after initialization, numComments is set to 0 so getNumComments should return 0
+    assertEquals(0, commentCounter.getNumComments());
   }
   
   @Test
   public void testGetNumLines() {
-    
+    //getNumLines should return the number of comment lines
+    //in this case, after initialization, numLines is set to 0 go getNumLines should return 0
+    assertEquals(0, commentCounter.getNumLines());
   }
   
   @Test
   public void testIsCommentNodesRequired() {
+    //isCommentNodesRequired should always return true
     assertEquals(true, commentCounter.isCommentNodesRequired());
   }
   
   @Test
   public void testBeginTree() {
-    DetailAST ast = new DetailAST(); //replace with mock
-    commentCounter.beginTree(ast);
+    //when begin is tree is called, both numComments and numLines should be set to 0
+    commentCounter.beginTree(astMock);
     assertEquals(0, commentCounter.getNumComments());
     assertEquals(0, commentCounter.getNumLines());
   }
   
   @Test
   public void testGetAcceptableTokens() {
+    //the acceptable tokens array should only contain two entries: one for the BLOCK_COMMENT_BEGIN token type and one for the SINGLE_LINE_COMMENT token type
     int[] expected = { TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.SINGLE_LINE_COMMENT };
     int[] actual = commentCounter.getAcceptableTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -54,6 +70,7 @@ public class CommentCounterTest {
   
   @Test
   public void testGetDefaultTokens() {
+    //the default tokens array should only contain two entries: one for the BLOCK_COMMENT_BEGIN token type and one for the SINGLE_LINE_COMMENT token type
     int[] expected = { TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.SINGLE_LINE_COMMENT };
     int[] actual = commentCounter.getDefaultTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -62,6 +79,7 @@ public class CommentCounterTest {
   
   @Test
   public void testGetRequiredTokens() {
+    //the required tokens array should only contain two entries: one for the BLOCK_COMMENT_BEGIN token type and one for the SINGLE_LINE_COMMENT token type
     int[] expected = { TokenTypes.BLOCK_COMMENT_BEGIN, TokenTypes.SINGLE_LINE_COMMENT };
     int[] actual = commentCounter.getRequiredTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -70,7 +88,42 @@ public class CommentCounterTest {
   
   @Test
   public void testVisitToken() {
-    
+    //visit token should increment numComments and numCommentLines both by 1 if the token type is SINGLE_LINE_COMMENT
+    Mockito.when(astMock.getType()).thenReturn(TokenTypes.SINGLE_LINE_COMMENT);
+    commentCounter.visitToken(astMock);
+    assertEquals(1, commentCounter.getNumComments());
+    assertEquals(1, commentCounter.getNumLines());
+    //visit token should increment numCommentLines by the number of comment lines a block comment covers if the token type is BLOCK_COMMENT_END
+    //visit token should increment numComments by 1 when the token type is BLOCK_COMMENT_BEGIN
+    DetailAST astChildMock = PowerMockito.mock(DetailAST.class);
+    Mockito.when(astMock.getType()).thenReturn(TokenTypes.BLOCK_COMMENT_BEGIN);
+    Random random = new Random();
+    int lineNoStart = random.nextInt();
+    int lineNoEnd = 0;
+    while(lineNoEnd < lineNoStart) {
+      lineNoEnd = random.nextInt();
+    }
+    assertEquals(true, lineNoEnd > lineNoStart);
+    Mockito.when(astMock.getLineNo()).thenReturn(lineNoStart);
+    Mockito.when(astChildMock.getLineNo()).thenReturn(lineNoEnd);
+    Mockito.when(astMock.findFirstToken(TokenTypes.BLOCK_COMMENT_END)).thenReturn(astChildMock);
+    int expectedNumLines = lineNoEnd - lineNoStart + 1 + commentCounter.getNumLines();
+    commentCounter.visitToken(astMock);
+    assertEquals(expectedNumLines, commentCounter.getNumLines());
+    assertEquals(2, commentCounter.getNumComments());
+    //visit token should not increment numComments or numCommentLines if the token type is not a BLOCK_COMMENT_BEGIN or SINGLE_LINE_COMMENT
+    boolean isNotAcceptableToken = false;
+    int token = -1;
+    while(isNotAcceptableToken == false) { //this loop will generate a random token that is not a SINGLE_LINE_COMMENT or a BLOCK_COMMENT_BEGIN
+      token = random.nextInt(TokenTypes.WILDCARD_TYPE);
+      if(TokenTypes.SINGLE_LINE_COMMENT != token && TokenTypes.BLOCK_COMMENT_BEGIN != token) {
+        isNotAcceptableToken = true;
+      }
+    }
+    Mockito.when(astMock.getType()).thenReturn(token);
+    commentCounter.visitToken(astMock);
+    assertEquals(2, commentCounter.getNumComments());
+    assertEquals(expectedNumLines, commentCounter.getNumLines());
   }
   
   @Test
--- eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/VariableCounterTest.java
+++ eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/VariableCounterTest.java
@@ -14,6 +14,7 @@ import net.sf.eclipsecs.sample.checks.VariableCounter;
 import static junit.framework.TestCase.assertEquals;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
@@ -32,11 +33,14 @@ public class VariableCounterTest {
   
   @Test
   public void testGetNumVariables() {
+    //getNumVariables should return the number of variables 
+    //in this case, after initialization, numVariables is set to 0 so getNumVariables should return 0
     assertEquals(0, variableCounter.getNumVariables());
   }
   
   @Test
   public void testGetAcceptableTokens() {
+    //the acceptable tokens array should only contain one entry referring to the VARIABLE_DEF token type
     int[] expected = {TokenTypes.VARIABLE_DEF};
     int[] actual = variableCounter.getAcceptableTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -45,6 +49,7 @@ public class VariableCounterTest {
   
   @Test
   public void testGetDefaultTokens() {
+    //the default tokens array should only contain one entry referring to the VARIABLE_DEF token type
     int[] expected = {TokenTypes.VARIABLE_DEF};
     int[] actual = variableCounter.getDefaultTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -53,6 +58,7 @@ public class VariableCounterTest {
   
   @Test
   public void testGetRequiredTokens() {
+    //the required tokens array should only contain one entry referring to the VARIABLE_DEF token type
     int[] expected = {TokenTypes.VARIABLE_DEF};
     int[] actual = variableCounter.getRequiredTokens();
     boolean isEqual = Arrays.equals(expected, actual);
@@ -66,7 +72,16 @@ public class VariableCounterTest {
     variableCounter.visitToken(astMock);
     assertEquals(1, variableCounter.getNumVariables());
     //visit token should not increment numVariables if the token type is not a variable definition
-    Mockito.when(astMock.getType()).thenReturn(TokenTypes.ABSTRACT);
+    boolean isNotAcceptableToken = false;
+    int token = -1;
+    Random random = new Random();
+    while(isNotAcceptableToken == false) { //this loop will generate a random token that is not a VARIABLE_DEF
+      token = random.nextInt(TokenTypes.WILDCARD_TYPE);
+      if(TokenTypes.VARIABLE_DEF != token) {
+        isNotAcceptableToken = true;
+      }
+    }
+    Mockito.when(astMock.getType()).thenReturn(token);
     variableCounter.visitToken(astMock);
     assertEquals(1, variableCounter.getNumVariables());
   }
