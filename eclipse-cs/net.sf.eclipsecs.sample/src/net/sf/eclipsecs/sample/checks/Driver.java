e74bd0f1ebcf5c3a00a4140e6cecf5a1c2f1484c
krhodes <kayla.rhodes@wsu.edu>
2018-10-21 16:28:50 -0700

    Driver refactor to make integration testing easier

--- eclipse-cs/net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/Driver.java
+++ eclipse-cs/net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/Driver.java
@@ -5,37 +5,38 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class Driver 
 {
+  
+  public CastCounter castCounter;
+  public CommentCounter commentCounter;
+  public ExpressionCounter expressionCounter;
+  public HalsteadMetrics halsteadMetrics;
+  public LoopingStatementCounter loopingStatementCounter;
+  public MaintainabilityIndex maintainabilityIndex;
+  public MethodCounter methodCounter;
+  public VariableCounter variableCounter;
     public static void main( String[] args )
     {
-      Driver driver = new Driver();
-      CastCounter castCounter = new CastCounter();
-      CommentCounter commentCounter = new CommentCounter();
-      ExpressionCounter expressionCounter = new ExpressionCounter();
-      HalsteadMetrics halsteadMetrics = new HalsteadMetrics();
-      LoopingStatementCounter loopingStatementCounter = new LoopingStatementCounter();
-      MaintainabilityIndex maintainabilityIndex = new MaintainabilityIndex();
-      MethodCounter methodCounter = new MethodCounter();
-      VariableCounter variableCounter = new VariableCounter();
+      Driver driver = new Driver(new CastCounter(), new CommentCounter(), new ExpressionCounter(), new HalsteadMetrics(), new LoopingStatementCounter(), new MaintainabilityIndex(), new MethodCounter(), new VariableCounter());
       DetailAST detailAST = new DetailAST();
       
       detailAST.setType(TokenTypes.TYPECAST);
-      int numCasts = driver.runCastCounter(castCounter, detailAST);
+      int numCasts = driver.runCastCounter(detailAST);
       System.out.println("Number of casts = " + numCasts);
       
       detailAST.setType(TokenTypes.SINGLE_LINE_COMMENT);
-      int[] commentCounterResults = driver.runCommentCounter(commentCounter, detailAST);
+      int[] commentCounterResults = driver.runCommentCounter(detailAST);
       System.out.println("Number of comments = " + commentCounterResults[0]);
       System.out.println("Number of comment lines = " + commentCounterResults[1]);
       
       detailAST.setType(TokenTypes.ABSTRACT);
-      int[] expressionCounterResults = driver.runExpressionCounter(expressionCounter, detailAST);
+      int[] expressionCounterResults = driver.runExpressionCounter(detailAST);
       System.out.println("Number of expressions = " + expressionCounterResults[0]);
       System.out.println("Number of operators = " + expressionCounterResults[1]);
       System.out.println("Number of operands = " + expressionCounterResults[2]);
       System.out.println("Number of unique operators = " + expressionCounterResults[3]);
       System.out.println("Number of unique operands = " + expressionCounterResults[4]);
       
-      double[] halsteadMetricsResults = driver.runHalsteadMetrics(halsteadMetrics, detailAST);
+      double[] halsteadMetricsResults = driver.runHalsteadMetrics(detailAST);
       System.out.println("Halstead length = " + halsteadMetricsResults[0]);
       System.out.println("Halstead vocab = " + halsteadMetricsResults[1]);
       System.out.println("Halstead volume = " + halsteadMetricsResults[2]);
@@ -43,33 +44,44 @@ public class Driver
       System.out.println("Halstead effort = " + halsteadMetricsResults[4]);
       
       detailAST.setType(TokenTypes.LITERAL_FOR);
-      int numLooping = driver.runLoopingStatementCounter(loopingStatementCounter, detailAST);
+      int numLooping = driver.runLoopingStatementCounter(detailAST);
       System.out.println("Number of looping statements = " + numLooping);
       
       detailAST.setType(TokenTypes.ABSTRACT);
-      double mIndex = driver.runMaintainabilityIndex(maintainabilityIndex, detailAST);
+      double mIndex = driver.runMaintainabilityIndex(detailAST);
       System.out.println("Maintainability index = " + mIndex);
       
       detailAST.setType(TokenTypes.METHOD_CALL);
       DetailAST child = new DetailAST();
       child.setType(TokenTypes.DOT);
       detailAST.setFirstChild(child);
-      int[] methodCounterResults = driver.runMethodCounter(methodCounter, detailAST);
+      int[] methodCounterResults = driver.runMethodCounter(detailAST);
       System.out.println("Number of external references = " + methodCounterResults[0]);
       System.out.println("Number of local references = " + methodCounterResults[1]);
       
       detailAST.setType(TokenTypes.VARIABLE_DEF);
-      int numVariables = driver.runVariableCounter(variableCounter, detailAST);
+      int numVariables = driver.runVariableCounter(detailAST);
       System.out.println("Number of variables = " + numVariables);
     }
     
-    public int runCastCounter(CastCounter castCounter, DetailAST detailAST) {
+    public Driver(CastCounter castCounter, CommentCounter commentCounter, ExpressionCounter expressionCounter, HalsteadMetrics halsteadMetrics, LoopingStatementCounter loopingStatementCounter, MaintainabilityIndex maintainabilityIndex, MethodCounter methodCounter, VariableCounter variableCounter) {
+      this.castCounter = castCounter;
+      this.commentCounter = commentCounter;
+      this.expressionCounter = expressionCounter;
+      this.halsteadMetrics = halsteadMetrics;
+      this.loopingStatementCounter = loopingStatementCounter;
+      this.maintainabilityIndex = maintainabilityIndex;
+      this.methodCounter = methodCounter;
+      this.variableCounter = variableCounter;
+    }
+    
+    public int runCastCounter(DetailAST detailAST) {
       castCounter.beginTree(detailAST);
       castCounter.visitToken(detailAST);
       return castCounter.getNumCasts();
     }
     
-    public int[] runCommentCounter(CommentCounter commentCounter, DetailAST detailAST) {
+    public int[] runCommentCounter(DetailAST detailAST) {
       commentCounter.beginTree(detailAST);
       commentCounter.visitToken(detailAST);
       int[] results = new int[2];
@@ -78,7 +90,7 @@ public class Driver
       return results;
     }
     
-    public int[] runExpressionCounter(ExpressionCounter expressionCounter, DetailAST detailAST) {
+    public int[] runExpressionCounter(DetailAST detailAST) {
       expressionCounter.beginTree(detailAST);
       expressionCounter.visitToken(detailAST);
       int[] results = new int[6];
@@ -90,7 +102,7 @@ public class Driver
       return results;
     }
     
-    public double[] runHalsteadMetrics(HalsteadMetrics halsteadMetrics, DetailAST detailAST) {
+    public double[] runHalsteadMetrics(DetailAST detailAST) {
       halsteadMetrics.beginTree(detailAST);
       halsteadMetrics.visitToken(detailAST);
       double[] results = new double[6];
@@ -102,19 +114,19 @@ public class Driver
       return results;
      }
     
-    public int runLoopingStatementCounter(LoopingStatementCounter loopingStatementCounter, DetailAST detailAST) {
+    public int runLoopingStatementCounter(DetailAST detailAST) {
       loopingStatementCounter.beginTree(detailAST);
       loopingStatementCounter.visitToken(detailAST);
       return loopingStatementCounter.getNumLoopingStatements();
     }
     
-    public double runMaintainabilityIndex(MaintainabilityIndex maintainabilityIndex, DetailAST detailAST) {
+    public double runMaintainabilityIndex(DetailAST detailAST) {
       maintainabilityIndex.beginTree(detailAST);
       maintainabilityIndex.visitToken(detailAST);
       return maintainabilityIndex.getMaintainabilityIndex();
     }
     
-    public int[] runMethodCounter(MethodCounter methodCounter, DetailAST detailAST) {
+    public int[] runMethodCounter(DetailAST detailAST) {
       methodCounter.beginTree(detailAST);
       methodCounter.visitToken(detailAST);
       int[] results = new int[3];
@@ -123,7 +135,7 @@ public class Driver
       return results;
     }
     
-    public int runVariableCounter(VariableCounter variableCounter, DetailAST detailAST) {
+    public int runVariableCounter(DetailAST detailAST) {
       variableCounter.beginTree(detailAST);
       variableCounter.visitToken(detailAST);
       return variableCounter.getNumVariables();
