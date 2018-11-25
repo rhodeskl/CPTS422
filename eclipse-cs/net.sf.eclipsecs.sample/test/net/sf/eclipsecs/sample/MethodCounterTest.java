3da161689aafd255518a81b480fad57619e8d126
krhodes <kayla.rhodes@wsu.edu>
2018-11-24 18:56:36 -0800

    added a few new test cases for method counter

--- eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/MethodCounterTest.java
+++ eclipse-cs/net.sf.eclipsecs.sample/test/net/sf/eclipsecs/sample/MethodCounterTest.java
@@ -95,14 +95,67 @@ public class MethodCounterTest {
     methodCounter.visitToken(astMock);
     
     assertEquals(1, methodCounter.getNumExternalReferences());
+    assertEquals(0, methodCounter.getNumLocalReferences()); //added as result of deliverable 3 analysis
     
     Mockito.when(astMock.getType()).thenReturn(TokenTypes.METHOD_CALL);  
     Mockito.when(astMock.getFirstChild()).thenReturn(astChildMock);
-    Mockito.when(astChildMock.getType()).thenReturn(TokenTypes.EMPTY_STAT);
+    int token = getRandomizedToken(TokenTypes.DOT); //added as result of deliverable 3 analysis
+    Mockito.when(astChildMock.getType()).thenReturn(token/*TokenTypes.EMPTY_STAT*/); //the return value was changed as result of deliverable 3 analysis
     methodCounter.visitToken(astMock);
     
     assertEquals(1, methodCounter.getNumLocalReferences());
+    assertEquals(1, methodCounter.getNumExternalReferences()); //added as a result of deliverable 3 analysis
+    
+    //deliverable 3 analysis test case 3
+    token = getRandomizedToken(TokenTypes.METHOD_CALL);
+    Mockito.when(astMock.getType()).thenReturn(token);
+    Mockito.when(astChildMock.getType()).thenReturn(TokenTypes.DOT);
+    methodCounter.visitToken(astMock);
+    
+    assertEquals(1, methodCounter.getNumLocalReferences());
+    assertEquals(1, methodCounter.getNumExternalReferences());
+    
+    //deliverable 3 analysis test case 4
+    token = getRandomizedToken(TokenTypes.METHOD_CALL);
+    Mockito.when(astMock.getType()).thenReturn(token);
+    token = getRandomizedToken(TokenTypes.DOT);
+    Mockito.when(astChildMock.getType()).thenReturn(token);
+    methodCounter.visitToken(astMock);
+    
+    assertEquals(1, methodCounter.getNumLocalReferences());
+    assertEquals(1, methodCounter.getNumExternalReferences());
+    
+    /*
+    //deliverable 3 analysis test case 5
+    Mockito.when(astMock.getType()).thenReturn(TokenTypes.METHOD_CALL);
+    Mockito.when(astMock.getFirstChild()).thenReturn(null);
+    methodCounter.visitToken(astMock);
+    
+    assertEquals(2, methodCounter.getNumLocalReferences());
+    assertEquals(1, methodCounter.getNumExternalReferences());
+    
+    //deliverable 3 analysis test case 6
+    token = getRandomizedToken(TokenTypes.METHOD_CALL);
+    Mockito.when(astMock.getType()).thenReturn(token);
+    methodCounter.visitToken(astMock);
+    
+    assertEquals(2, methodCounter.getNumLocalReferences());
+    assertEquals(1, methodCounter.getNumExternalReferences());*/
     
   }
+  
+//added for deliverable 3 test cases
+int getRandomizedToken(int type) {
+  Random random = new Random();
+  boolean isNotAcceptableToken = false;
+  int token = -1;
+  while(isNotAcceptableToken == false) {
+    token = random.nextInt(TokenTypes.WILDCARD_TYPE);
+    if(type != token) {
+      isNotAcceptableToken = true;
+    }
+  }
+  return token;
+}
 
 }
